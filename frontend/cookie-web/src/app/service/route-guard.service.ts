728daf66186620cee6aee2faf4cde4e904da3526
dscarrig <dcarrig1@gmail.com>
2025-11-12 20:23:46 -0600

    Update to Angular 16

--- frontend/cookie-web/src/app/service/route-guard.service.ts
+++ frontend/cookie-web/src/app/service/route-guard.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { BasicAuthenticationService } from './basic-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService implements CanActivate{
+export class RouteGuardService {
 
   constructor(
     private authenticationService: BasicAuthenticationService,
